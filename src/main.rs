4a0a842dc2318f4ae23683745ad26d08fabb270e
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-12-22 23:02:27 +1100

    Switch back to JSON over stdio

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,18 @@
-use anyhow::anyhow;
 use polyp::{Key, Ui, UserInput};
-use std::env;
-use tungstenite::Message;
-use url::Url;
+use std::io::{self, Write};
 
 fn main() -> anyhow::Result<()> {
-    let (mut server_websocket, _) = {
-        let server_socket_addr = env::args()
-            .nth(1)
-            .ok_or_else(|| anyhow!("expected argument of server socket address"))?;
-
-        let server_websocket_url = Url::parse(&format!("ws://{}", server_socket_addr))?;
-
-        tungstenite::client::connect(server_websocket_url)?
-    };
-
-    println!("kon-polyp: connected to server\r");
-
     let mut buffer_contents = String::new();
     let mut cursor_idx = 0;
 
     loop {
-        let pressed_key = match server_websocket.read_message()? {
-            Message::Binary(json) => {
-                let UserInput::PressedKey(pressed_character) = serde_json::from_slice(&json)?;
-                pressed_character
-            }
-            Message::Close(_) => return Ok(()),
-            _ => unreachable!(),
+        let UserInput::PressedKey(pressed_key) = {
+            let mut buf = String::new();
+            io::stdin().read_line(&mut buf)?;
+            serde_json::from_slice(buf.as_bytes())?
         };
-        println!("kon-polyp: user pressed ‘{:?}’\r", pressed_key);
+
+        eprintln!("kon-polyp: user pressed ‘{:?}’\r", pressed_key);
 
         match pressed_key {
             Key::Char(c) => {
@@ -45,14 +28,15 @@ fn main() -> anyhow::Result<()> {
             _ => {}
         }
 
-        println!("kon-polyp: buffer now is ‘{}’\r", buffer_contents);
+        eprintln!("kon-polyp: buffer now is ‘{}’\r", buffer_contents);
 
         let ui = Ui::TextField {
             current_text: buffer_contents.clone(),
             cursor_idx,
         };
         let serialized = serde_json::to_vec(&ui)?;
-        server_websocket.write_message(Message::Binary(serialized))?;
-        println!("kon-polyp: wrote UI to server\r");
+        io::stdout().write_all(&serialized)?;
+        io::stdout().write_all(b"\n")?;
+        eprintln!("kon-polyp: wrote UI to server\r");
     }
 }
